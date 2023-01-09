0618af227ab1bff9ad6da60dff5ce2cd2b2f8467
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2023-01-09 23:29:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = st.nextToken().charAt(0);
+
+        System.out.println(a);
+        
+    }
+}
