67f680f31d5ea00390f1af6b5856feb778937563
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2023-01-25 11:00:20 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class  Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+
+        System.out.println(A+"??!");
+
+        }
+    }
